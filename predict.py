5972c05e5a25dd6956a3cfc3f2d5501622da6acc
Mehul <mpsampat@gmail.com>
2018-04-03 02:23:20 +0000

    predict file for best result of 0.44

---
+++ predict.py
@@ -0,0 +1,255 @@
+#!/usr/bin/env python
+import os
+import sys
+sys.path.append('Mask_RCNN')
+import random
+import math
+import re
+import time
+
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+import csv
+import imageio
+import skimage
+from config import Config
+from skimage.morphology import label
+from skimage.feature import canny
+from skimage import exposure
+from keras.callbacks import Callback
+from skimage.morphology import binary_closing, binary_opening, disk, binary_dilation
+from scipy.ndimage.morphology import binary_fill_holes
+from sklearn.externals import joblib
+from skimage.transform import PiecewiseAffineTransform, warp
+from skimage.morphology import watershed
+from skimage.filters import sobel
+
+import utils
+import model as modellib
+import visualize
+from model import log
+import sys
+sys.path.append('Mask_RCNN')
+##############################################
+# Root directory of the project
+ROOT_DIR = os.getcwd()
+
+# Directory to save logs and trained model
+MODEL_DIR = os.path.join(ROOT_DIR, "logs")
+
+# Local path to trained weights file
+COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
+# Download COCO trained weights from Releases if needed
+if not os.path.exists(COCO_MODEL_PATH):
+    utils.download_trained_weights(COCO_MODEL_PATH)
+##############################################
+class DsbDataset(utils.Dataset):
+
+    def load_dataset(self, ids, train_mode=True):
+        self.add_class("dsb", 1, "nuclei")
+        if train_mode:
+            directory = dsb_dir
+        else:
+            directory = test_dir
+        for i, id in enumerate(ids):
+            image_dir = os.path.join(directory, id)
+            self.add_image("dsb", image_id=i, path=image_dir)
+            
+
+    def load_image(self, image_id, non_zero=None):
+        info = self.image_info[image_id]
+        path = info['path']
+        image_name = os.listdir(os.path.join(path, 'images'))
+        image_path = os.path.join(path, 'images', image_name[0])
+        image = imageio.imread(image_path)
+        if image.shape[2] != 3:
+            image = image[:,:,:3]
+        image = self.preprocess(image)
+        image = image.astype('float32')
+        return image
+
+    def image_reference(self, image_id):
+        info = self.image_info[image_id]
+        if info["source"] == "shapes":
+            return info["shapes"]
+        else:
+            super(self.__class__).image_reference(self, image_id)
+
+    def load_mask(self, image_id):
+        info = self.image_info[image_id]
+        path = info['path']
+        mask_dir = os.path.join(path, 'masks')
+        mask_names = os.listdir(mask_dir)
+        count = len(mask_names)
+        mask = []
+        for i, el in enumerate(mask_names):
+            msk_path = os.path.join(mask_dir, el)
+            msk = imageio.imread(msk_path)
+            if np.sum(msk) == 0:
+                print('invalid mask')
+                continue
+            msk = msk.astype('float32')/255.
+            mask.append(msk)
+        mask = np.asarray(mask)
+        mask[mask > 0.] = 1.
+        mask = np.transpose(mask, (1,2,0))
+        occlusion = np.logical_not(mask[:, :, -1]).astype(np.uint8)
+        count = mask.shape[2]
+        for i in range(count-2, -1, -1):
+            mask[:, :, i] = mask[:, :, i] * occlusion
+            occlusion = np.logical_and(occlusion, np.logical_not(mask[:, :, i]))
+        class_ids = [self.class_names.index('nuclei') for s in range(count)]
+        class_ids = np.asarray(class_ids)
+        return mask, class_ids.astype(np.int32)
+    
+    def preprocess(self, img):
+        gray = skimage.color.rgb2gray(img.astype('uint8'))
+        img = skimage.color.gray2rgb(gray)
+        mean = np.mean(img)
+        std  = np.std(img)
+        #img = (img-mean)/std        
+        img = img*255.
+        return img
+##############################################
+class DsbConfig(Config):
+
+    # Give the configuration a recognizable name
+    NAME = "dsb"
+      
+    LEARNING_RATE = 1e-03
+    
+    # If enabled, resizes instance masks to a smaller size to reduce
+    # memory load. Recommended when using high-resolution image
+    USE_MINI_MASK = True
+    MINI_MASK_SHAPE = (56, 56)  # (height, width) of the mini-mask
+    
+    # Train on 1 GPU and 8 images per GPU. Batch size is GPUs * images/GPU.
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 2
+    # Total number of steps (batches of samples) to yield from generator before declaring one epoch finished and starting the next epoch.
+    # typically be equal to the number of samples of your dataset divided by the batch size
+    STEPS_PER_EPOCH = 300
+    VALIDATION_STEPS = 70
+  
+    # Number of classes (including background)
+    NUM_CLASSES = 1 + 1  # background + nucleis
+    IMAGE_MIN_DIM = 512
+    IMAGE_MAX_DIM = 512
+    IMAGE_PADDING = True  # currently, the False option is not supported
+    RPN_ANCHOR_SCALES = (8, 16, 32, 64, 128)  # anchor side in pixels, maybe add a 256?
+    # The strides of each layer of the FPN Pyramid. These values
+    # are based on a Resnet101 backbone.
+    BACKBONE="resnet101"
+    BACKBONE_STRIDES = [4, 8, 16, 32, 64]
+    # How many anchors per image to use for RPN training
+    RPN_TRAIN_ANCHORS_PER_IMAGE = 320 #300
+    
+    # ROIs kept after non-maximum supression (training and inference)
+    POST_NMS_ROIS_TRAINING = 2000
+    POST_NMS_ROIS_INFERENCE = 2000
+    POOL_SIZE = 7
+    MASK_POOL_SIZE = 14
+    MASK_SHAPE = [28, 28]
+    TRAIN_ROIS_PER_IMAGE = 512
+    RPN_NMS_THRESHOLD = 0.7
+    MAX_GT_INSTANCES = 256
+    DETECTION_MAX_INSTANCES = 500 
+    # Minimum probability value to accept a detected instance
+    # ROIs below this threshold are skipped
+    DETECTION_MIN_CONFIDENCE = 0.7 # may be smaller?
+    # Non-maximum suppression threshold for detection
+    DETECTION_NMS_THRESHOLD = 0.3 # 0.3
+    
+    
+    MEAN_PIXEL = np.array([0.,0.,0.])
+    
+    # Weight decay regularization
+    WEIGHT_DECAY = 0.0001
+    
+#############################################
+#train_ids = meta_train.ImageId.values
+#val_ids = meta_valid.ImageId.values
+test_dir = '/public/dsb_2018_data/stage1_test'
+test_ids = os.listdir(test_dir)
+###############################################################
+
+# Test dataset
+dataset_test = DsbDataset()
+dataset_test.load_dataset(test_ids, train_mode=False)
+dataset_test.prepare()
+
+######################################################
+class InferenceConfig(DsbConfig):
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+    # MEAN_PIXEL = np.array([56.02288505, 54.02376286, 54.26675248])
+inference_config = InferenceConfig()
+model = modellib.MaskRCNN(mode="inference", 
+                          config=inference_config,
+                          model_dir=MODEL_DIR)
+#############################################################
+# Get path to saved weights
+# Either set a specific path or find last trained weights
+# model_path = os.path.join(ROOT_DIR, ".h5 file name here")
+model_path = model.find_last()[1]
+# Load trained weights (fill in path to trained weights here)
+assert model_path != "", "Provide path to trained weights"
+print("Loading weights from ", model_path)
+model.load_weights(model_path, by_name=True)
+#################################################################################
+raw_predictions = []
+for test_id in dataset_test.image_ids:
+    test_image1 = dataset_test.load_image(test_id, 0)
+    pred = model.detect([test_image1], verbose=0)
+    pred = pred[0]
+    sc = pred['scores']
+    pred = pred['masks']
+    raw_predictions.append((pred, sc))
+###################################################################################
+def rle_encoding(x):
+    '''
+    x: numpy array of shape (height, width), 1 - mask, 0 - background
+    Returns run length as list
+    '''
+    dots = np.where(x.T.flatten()==1)[0] # .T sets Fortran order down-then-right
+    run_lengths = []
+    prev = -2
+    for b in dots:
+        if (b>prev+1): run_lengths.extend((b+1, 0))
+        run_lengths[-1] += 1
+        prev = b
+    return run_lengths
+#########################################################################
+def numpy2encoding_no_overlap(predicts, img_name, scores):
+    sum_predicts = np.sum(predicts, axis=2)
+    rows, cols = np.where(sum_predicts>=2)
+    
+    for i in zip(rows, cols):
+        instance_indicies = np.where(np.any(predicts[i[0],i[1],:]))[0]
+        highest = instance_indicies[0]
+        predicts[i[0],i[1],:] = predicts[i[0],i[1],:]*0
+        predicts[i[0],i[1],highest] = 1
+    
+    ImageId = []
+    EncodedPixels = []
+    print(predicts.shape)
+    for i in range(predicts.shape[2]): 
+        rle = rle_encoding(predicts[:,:,i])
+        if len(rle)>0:
+            ImageId.append(img_name)
+            EncodedPixels.append(rle)    
+    return ImageId, EncodedPixels
+##########################################################################
+
+new_test_ids = []
+rles = []
+for id, raw_pred in zip(test_ids, raw_predictions):
+    ids, rle = numpy2encoding_no_overlap(raw_pred[0], id, raw_pred[1])
+    new_test_ids += ids
+    rles += rle
+########################################################################
+df = pd.DataFrame({ 'ImageId' : new_test_ids , 'EncodedPixels' : rles})
+df.to_csv('/output/submission.csv', index=False, columns=['ImageId', 'EncodedPixels'])
